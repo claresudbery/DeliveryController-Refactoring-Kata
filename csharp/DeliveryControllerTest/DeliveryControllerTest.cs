5bcbfbf1bda3ab0bc1ee184101ca90600166f7d6
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-04-23 23:07:30 +0100

    add more test data

--- csharp/DeliveryControllerTest/DeliveryControllerTest.cs
+++ csharp/DeliveryControllerTest/DeliveryControllerTest.cs
@@ -7,27 +7,52 @@ namespace DeliveryControllerTest
 {
     public class DeliveryControllerTest
     {
-        private readonly Delivery _salmonDelivery;
+        private readonly Delivery _salmonDelivery01;
+        private readonly Delivery _salmonDelivery02;
         private readonly List<Delivery> _testDeliverySchedule;
-        private readonly DeliveryEvent _salmonDeliveryEvent;
-        private const string SalmonDeliveryId = "SalmonDelivery01";
+        private readonly DeliveryEvent _salmonDeliveryEvent01;
+        private readonly DeliveryEvent _salmonDeliveryEvent02;
+        private readonly DateTime _salmonDeliveryTime01 = new DateTime(2023, 12, 31, 14, 05, 0);
+        private readonly DateTime _salmonDeliveryTime02 = new DateTime(2023, 12, 31, 15, 07, 0);
+        private readonly Location _salmonDeliveryLocation = new Location((float)12.2, (float)13.3);
+        private const string SalmonDeliveryId01 = "SalmonDelivery01";
+        private const string SalmonDeliveryId02 = "SalmonDelivery02";
+        private bool _emailSent = false;
+        private bool _averageSpeedUpdated = false;
 
         public DeliveryControllerTest()
         {
-            var salmonDeliveryTime = new DateTime(2023, 12, 31);
-            var salmonDeliveryLocation = new Location((float)12.2, (float)13.3);
-            _salmonDelivery = new Delivery(
-                id: SalmonDeliveryId,
+            _salmonDelivery01 = new Delivery(
+                id: SalmonDeliveryId01,
                 contactEmail: "Sally@Sally.co.uk",
-                location: salmonDeliveryLocation,
-                timeOfDelivery: salmonDeliveryTime,
+                location: _salmonDeliveryLocation,
+                timeOfDelivery: _salmonDeliveryTime01,
                 arrived: false,
                 onTime: false);
-            _testDeliverySchedule = new List<Delivery> { _salmonDelivery };
-            _salmonDeliveryEvent = new DeliveryEvent(
-                id: SalmonDeliveryId, 
-                timeOfDelivery: salmonDeliveryTime, 
-                location: salmonDeliveryLocation);
+            _salmonDelivery02 = new Delivery(
+                id: SalmonDeliveryId02,
+                contactEmail: "Sally@Sally.co.uk",
+                location: _salmonDeliveryLocation,
+                timeOfDelivery: _salmonDeliveryTime02,
+                arrived: false,
+                onTime: false);
+            _testDeliverySchedule = new List<Delivery> { _salmonDelivery01, _salmonDelivery02 };
+        }
+
+        private DeliveryEvent MakeSalmonDeliveryEvent01(DateTime timeOfDelivery)
+        {
+            return new DeliveryEvent(
+                id: SalmonDeliveryId01, 
+                timeOfDelivery: timeOfDelivery, 
+                location: _salmonDeliveryLocation);
+        }
+
+        private DeliveryEvent MakeSalmonDeliveryEvent02(DateTime timeOfDelivery)
+        {
+            return new DeliveryEvent(
+                id: SalmonDeliveryId02, 
+                timeOfDelivery: timeOfDelivery, 
+                location: _salmonDeliveryLocation);
         }
 
         [Fact]
@@ -38,15 +63,15 @@ namespace DeliveryControllerTest
                 _testDeliverySchedule, 
                 new EmailGateway(), 
                 new MapService());
-            _salmonDelivery.Arrived = false;
-            var initialArrivedValue = _salmonDelivery.Arrived;
+            _salmonDelivery01.Arrived = false;
+            var initialArrivedValue = _salmonDelivery01.Arrived;
             
             // Act
-            controller.UpdateDelivery(_salmonDeliveryEvent);
+            controller.UpdateDelivery(MakeSalmonDeliveryEvent01(_salmonDeliveryTime01));
             
             // Assert
-            Assert.True(_salmonDelivery.Arrived);
-            Assert.NotEqual(initialArrivedValue, _salmonDelivery.Arrived);
+            Assert.True(_salmonDelivery01.Arrived);
+            Assert.NotEqual(initialArrivedValue, _salmonDelivery01.Arrived);
         }
     }
 }
