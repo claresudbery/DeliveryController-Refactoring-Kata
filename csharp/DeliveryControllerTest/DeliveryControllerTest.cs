2c909bb68b31bf6ed23a8008d1b6c55e78e39497
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-04-23 22:45:23 +0100

    add test for average speed, one delivery on time

--- csharp/DeliveryControllerTest/DeliveryControllerTest.cs
+++ csharp/DeliveryControllerTest/DeliveryControllerTest.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace DeliveryControllerTest
 {
-    public class DeliveryControllerTest: IEmailGateway
+    public class DeliveryControllerTest: IEmailGateway, IMapService
     {
         private readonly Delivery _salmonDelivery;
         private readonly List<Delivery> _testDeliverySchedule;
@@ -14,6 +14,7 @@ namespace DeliveryControllerTest
         private readonly Location _salmonDeliveryLocation = new Location((float)12.2, (float)13.3);
         private const string SalmonDeliveryId = "SalmonDelivery01";
         private bool _emailSent = false;
+        private bool _averageSpeedUpdated = false;
 
         public DeliveryControllerTest()
         {
@@ -114,9 +115,37 @@ namespace DeliveryControllerTest
             Assert.NotEqual(_emailSent, initialEmailStatus);
         }
 
+        [Fact]
+        public void Test_UpdateDelivery_DoesNotUpdateAverageSpeed_WhenDeliveryOnTime()
+        {
+            // Arrange
+            var controller = new DeliveryController.DeliveryController(
+                _testDeliverySchedule, 
+                this, 
+                new MapService());
+            _averageSpeedUpdated = false;
+            
+            // Act
+            controller.UpdateDelivery(_salmonDeliveryEvent);
+            
+            // Assert
+            Assert.False(_averageSpeedUpdated);
+        }
+
         void IEmailGateway.Send(string address, string subject, string message)
         {
             _emailSent = true;
+        }
+
+        public double CalculateEta(Location location1, Location location2)
+        {
+            // Do nothing
+            return 0;
+        }
+
+        void IMapService.UpdateAverageSpeed(Location location1, Location location2, TimeSpan elapsedTime)
+        {
+            _averageSpeedUpdated = true;
         }
     }
 }
