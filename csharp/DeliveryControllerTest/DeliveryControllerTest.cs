e04559406b84548ef24d51592502e442d013cf25
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-04-23 21:09:02 +0100

    Add test data and starter test

--- csharp/DeliveryControllerTest/DeliveryControllerTest.cs
+++ csharp/DeliveryControllerTest/DeliveryControllerTest.cs
@@ -1,15 +1,52 @@
 using System;
+using System.Collections.Generic;
+using DeliveryController;
 using Xunit;
 
 namespace DeliveryControllerTest
 {
     public class DeliveryControllerTest
     {
-        [Fact]
-        public void Test1()
+        private readonly Delivery _salmonDelivery;
+        private readonly List<Delivery> _testDeliverySchedule;
+        private readonly DeliveryEvent _salmonDeliveryEvent;
+        private const string SalmonDeliveryId = "SalmonDelivery01";
+
+        public DeliveryControllerTest()
         {
-            // TODO: test DeliveryController.UpdateDelivery
-            Assert.Equal("foo", "not foo");
+            var salmonDeliveryTime = new DateTime(2023, 12, 31);
+            var salmonDeliveryLocation = new Location((float)12.2, (float)13.3);
+            _salmonDelivery = new Delivery(
+                id: SalmonDeliveryId,
+                contactEmail: "Sally@Sally.co.uk",
+                location: salmonDeliveryLocation,
+                timeOfDelivery: salmonDeliveryTime,
+                arrived: false,
+                onTime: false);
+            _testDeliverySchedule = new List<Delivery> { _salmonDelivery };
+            _salmonDeliveryEvent = new DeliveryEvent(
+                id: SalmonDeliveryId, 
+                timeOfDelivery: salmonDeliveryTime, 
+                location: salmonDeliveryLocation);
+        }
+
+        [Fact]
+        public void Test_UpdateDelivery_UpdatesDelivery_ToArrived()
+        {
+            // Arrange
+            var controller = new DeliveryController.DeliveryController(
+                _testDeliverySchedule, 
+                new EmailGateway(), 
+                new MapService());
+            _salmonDelivery.Arrived = false;
+            var initialArrivedValue = _salmonDelivery.Arrived;
+            
+            // Act
+            controller.UpdateDelivery(_salmonDeliveryEvent);
+            
+            // Assert
+            Assert.True(_salmonDelivery.Arrived);
+            Assert.NotEqual(initialArrivedValue, _salmonDelivery.Arrived);
         }
     }
 }
