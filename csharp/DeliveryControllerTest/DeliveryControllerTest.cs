081025e7b556f9a72d8d4121bc968135695fcda9
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-07-21 23:09:44 +0100

    Added first failing test

--- csharp/DeliveryControllerTest/DeliveryControllerTest.cs
+++ csharp/DeliveryControllerTest/DeliveryControllerTest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using DeliveryController;
 using Xunit;
 
 namespace DeliveryControllerTest
@@ -6,9 +8,32 @@ namespace DeliveryControllerTest
     public class DeliveryControllerTest
     {
         [Fact]
-        public void Test1()
+        public void Test_UpdateDelivery_UpdatesDelivery_ToArrived()
         {
-            // TODO: test UpdateDelivery
+            // Arrange
+            var salmonDeliveryLocation = new Location((float)12.2, (float)13.3);
+            var salmonDeliveryId = "SalmonDelivery01";
+            var timeOfDelivery = new DateTime(2023, 12, 31, 14, 05, 0);
+            var salmonDelivery = new Delivery(
+                id: salmonDeliveryId,
+                contactEmail: "Sally@Sally.co.uk",
+                location: salmonDeliveryLocation,
+                timeOfDelivery: timeOfDelivery,
+                arrived: false,
+                onTime: false);
+            List<Delivery> testDeliverySchedule = new List<Delivery>{salmonDelivery};
+            var controller = new DeliveryController.DeliveryController(
+                testDeliverySchedule);
+            
+            // Act
+            var deliveryEvent = new DeliveryEvent(
+                id: salmonDeliveryId, 
+                timeOfDelivery: timeOfDelivery, 
+                location: salmonDeliveryLocation);
+            controller.UpdateDelivery(deliveryEvent);
+            
+            // Assert
+            Assert.True(salmonDelivery.Arrived);
         }
     }
 }
