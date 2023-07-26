419fccfe1899f398206f1ebc690cd30cc79153ba
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-07-26 12:31:25 +0100

    Remove other languages to make demo code easier to find

--- kotlin/src/main/kotlin/kata/DeliveryEvent.kt
+++
@@ -1,9 +0,0 @@
-package kata
-
-import java.time.LocalDateTime
-
-class DeliveryEvent(
-    val id: String,
-    val timeOfDelivery: LocalDateTime,
-    val location: Location
-)
--- typescript/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts', '!**/*.d.ts'],
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/']
-};
